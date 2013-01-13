a3ccb9f4a78bd20903b6aaaec7e7ac0cbb8eefc3
Carlos Morgado <chbm@primatas.org>
2013-01-13 17:24:08 +0000

    * add memory to tests

--- test/test.js
+++ test/test.js
@@ -6,80 +6,81 @@ var assert = require('assert')
 
 'use strict';
 
-vows.describe('basic tests').addBatch({
-	'a store': {
-		topic: function () {
-			var vowsthis = this;
-			fs.unlink('/tmp/teststore', function (err) {
-				Persistance.make({dbtype: 'sqlite3', dbname: '/tmp/teststore'}, vowsthis.callback);
-			});
-		},
-		'is a object': function (err, store) {
-			assert.isObject(store);
-		},
-		'which has define': function (err, store) {
-			assert.isFunction(store.define);
-		},
-		'a defined object': {
-			topic: function (store) {
-				return store.define('foo', {
-					i: 1,
-					s: 'helo',
-					a: [],
-					o: {}
+['sqlite3', 'memory'].forEach(function (st) {
+	vows.describe(st+' tests').addBatch({
+		'a store': {
+			topic: function () {
+				var vowsthis = this;
+				fs.unlink('/tmp/teststore', function (err) {
+					Persistance.make({dbtype: st, dbname: '/tmp/teststore'}, vowsthis.callback);
 				});
 			},
-			'is a object': function (foo) {
-				assert.isObject(foo);
+			'is a object': function (err, store) {
+				assert.isObject(store);
 			},
-			'which has a schema': function (foo) {
-				assert.isObject(foo._persistschema);
+			'which has define': function (err, store) {
+				assert.isFunction(store.define);
 			},
-			'and a type': function (foo) {
-				assert.include(foo._persistschema, '_persisttype');
-			},
-			'which is foo': function (foo) {
-				assert.equal(foo._persistschema._persisttype, 'foo');
-			},
-			'and a create function': function (foo) {
-				assert.isFunction(foo.create);
-			},
-			'which returns an object': {
-				'topic': function (foo) { return foo.create(); },
-				'which can be saved': function (obj) {
-					assert.isFunction(obj.save);
+			'a defined object': {
+				topic: function (store) {
+					return store.define('foo', {
+						i: 1,
+						s: 'helo',
+						a: [],
+						o: {}
+					});
 				},
-				'and is initialized with defaults': function (obj) {
-					assert.equal(obj.i, 1);
-					assert.equal(obj.s, 'helo');
-					assert.deepEqual(obj.a, []);
-					assert.deepEqual(obj.o, {});
-				}
-			},
-			'if you create a named object': {
-				'topic': function (foo) {
-					foo.create('xpto').save();
-					foo.get('xpto', this.callback);
+				'is a object': function (foo) {
+					assert.isObject(foo);
 				},
-				'you can get it back': function (err, data) {
-					assert.isNull(err);
-					assert.isObject(data);
-					assert.isObject(data.xpto);
-				}
-			},
-			'you can search for it': {
-				'topic': function (foo) {
-					foo.search({i: 1}, this.callback);
+				'which has a schema': function (foo) {
+					assert.isObject(foo._persistschema);
 				},
-				'and get it back': function (err, data) {
-					assert.isNull(err);
-					assert.isObject(data);
-					assert.isObject(data.xpto);
+				'and a type': function (foo) {
+					assert.include(foo._persistschema, '_persisttype');
+				},
+				'which is foo': function (foo) {
+					assert.equal(foo._persistschema._persisttype, 'foo');
+				},
+				'and a create function': function (foo) {
+					assert.isFunction(foo.create);
+				},
+				'which returns an object': {
+					'topic': function (foo) { return foo.create(); },
+					'which can be saved': function (obj) {
+						assert.isFunction(obj.save);
+					},
+					'and is initialized with defaults': function (obj) {
+						assert.equal(obj.i, 1);
+						assert.equal(obj.s, 'helo');
+						assert.deepEqual(obj.a, []);
+						assert.deepEqual(obj.o, {});
+					}
+				},
+				'if you create a named object': {
+					'topic': function (foo) {
+						foo.create('xpto').save(function () {});
+						foo.get('xpto', this.callback);
+					},
+					'you can get it back': function (err, data) {
+						assert.isNull(err);
+						assert.isObject(data);
+						assert.isObject(data.xpto);
+					}
+				},
+				'you can search for it': {
+					'topic': function (foo) {
+						foo.search({i: 1}, this.callback);
+					},
+					'and get it back': function (err, data) {
+						assert.isNull(err);
+						assert.isObject(data);
+						assert.isObject(data.xpto);
+					}
 				}
 			}
+
 		}
 
-	}
-
-}).export(module);
-
+	}).export(module);
+});
